e232e7dd5b10b0df9098ddb74fdd2e01e1d1b85a
hisissh <2250847255@qq.com>
2020-12-02 15:36:12 +0800

    Hou

---
+++ MyTeam/src/com/nnxy/team4/Houkaiteng.java
@@ -0,0 +1,16 @@
+package com.nnxy.team4;
+
+public class Houkaiteng {
+
+	
+	private int id;
+	public int getId() {
+	return id;
+	}
+	String name;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+	}
+
+}
